83dc5b82cb5f420d725c7be622c0c2ed4fb3c00c
Fridolin Pokorny <fridolin@redhat.com>
2018-12-19 18:25:00 +0100

    Fixes for deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
+set -x
+
+export CEPH_BUCKET=${CEPH_BUCKET:=DH-DEV-DATA}
+export CEPH_BUCKET_PREFIX={CEPH_BUCKET_PREFIX:=data/thoth/selinon/}
+[ -z "$S3_ENDPOINT_URL" -o -z "$S3_KEY_ID" -o -z "$S3_SECRET_KEY" ] && { echo "Please S3 configuration"; exit 1; }
+export S3_ENDPOINT_URL=${S3_ENDPOINT_URL}
+export S3_KEY_ID=${S3_KEY_ID}
+export S3_SECRET_KEY=${S3_SECRET_KEY}
 export SELINON_NAMESPACE=${SELINON_NAMESPACE:=fpokorny-thoth-dev}
 export OCP_URL=$(oc whoami --show-server)
 export OCP_TOKEN=$(oc whoami --show-token)
